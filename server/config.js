c587b788ac05afd18f8a525d9af96e4436ea569b
Jake Foraker <jake.foraker@byjakt.com>
2016-05-21 23:56:46 -0400

    adjust link style and add my gh url

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ const prod = process.env.NODE_ENV === 'production';
 
 const config = {
 	mongoURL: prod ? process.env.MONGO_URL : 'mongodb://localhost:27017/mern-starter',
-	port: process.env.PORT || 8000,
+	port: process.env.PORT || 8000
 };
 
 export default config;
--- shared/components/Footer/Footer.jsx
+++ shared/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
    <div className="footer">
-     <p>An experiment by <a href="https://github.com/jforaker" target="_Blank">@jakeforaker</a></p>
+     <p>An experiment by <a href="https://github.com/jforaker/mern-bite-url" target="_Blank">@jakeforaker</a></p>
    </div>
  );
 }
