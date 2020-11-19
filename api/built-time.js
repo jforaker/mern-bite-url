3032f951a15c01829845b3be1fce11908244660d
Jake Foraker <jakeforaker83@gmail.com>
2020-11-18 23:30:17 -0500

    Delete built-time.js

--- api/built-time.js
+++
@@ -1,8 +0,0 @@
-const BuiltTime = require("./built-time");
-module.exports = (req, res) => {
-  res.setHeader("content-type", "text/plain");
-  res.send(`
-    This Serverless Function was built at ${new Date(BuiltTime)}.
-    The current time is ${new Date()}
-  `);
-};
